c902fa59cc4300e225f1cb4499332a24382a8690
kocwng <idakusuma2705@gmail.com>
2023-06-08 18:41:42 +0700

    author:add response

--- features/authors/delivery/response.go
+++ features/authors/delivery/response.go
@@ -1,1 +1,25 @@
 package delivery
+
+import "online-library/features/authors"
+
+type AuthorResponse struct {
+	Id      uint   `json:"id"`
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+func ToResponse(authorEntity authors.AuthorEntity) AuthorResponse {
+	return AuthorResponse{
+		Id:      authorEntity.Id,
+		Name:    authorEntity.Name,
+		Country: authorEntity.Country,
+	}
+}
+
+func ToListResponse(authorEntity []authors.AuthorEntity) []AuthorResponse {
+	var response []AuthorResponse
+	for _, v := range authorEntity {
+		response = append(response, ToResponse(v))
+	}
+	return response
+}
