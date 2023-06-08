2ec7bb06364d2a9a1463911046b4d3d6a7d0907a
kocwng <idakusuma2705@gmail.com>
2023-06-08 18:24:46 +0700

    add request

--- features/authors/delivery/request.go
+++ features/authors/delivery/request.go
@@ -1,1 +1,15 @@
 package delivery
+
+import "online-library/features/authors"
+
+type AuthorRequest struct {
+	Name    string
+	Country string
+}
+
+func ToEntity(authorRequest AuthorRequest) authors.AuthorEntity {
+	return authors.AuthorEntity{
+		Name:    authorRequest.Name,
+		Country: authorRequest.Country,
+	}
+}
--- features/books/delivery/request.go
+++ features/books/delivery/request.go
@@ -1,1 +1,26 @@
 package delivery
+
+import (
+	"online-library/features/authors/delivery"
+	"online-library/features/books"
+)
+
+type BookRequest struct {
+	Title         string
+	PublishedYear int
+	ISBN          string
+	Author        []delivery.AuthorRequest
+}
+
+func ToEntity(bookRequest BookRequest) books.BookEntity {
+	result := books.BookEntity{
+		Title: bookRequest.Title,
+		PublishedYear: bookRequest.PublishedYear,
+		ISBN: bookRequest.ISBN,
+	}
+
+	for _, v := range bookRequest.Author{
+		result.Author = append(result.Author, delivery.ToEntity(v))
+	}
+	return result
+}
